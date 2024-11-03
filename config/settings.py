5faa7e4c840c842cedab5a2e5ec2f73ce2692730
Mohammad Khazaei <mkhazaei.dev@gmail.com>
2024-11-03 14:05:14 +0330

    Building a base app and creating basic pages to build project pages.

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Defined Apps
+    'base.apps.BaseConfig',
+
+    # Third Party Apps
 ]
 
 MIDDLEWARE = [
@@ -54,7 +59,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,6 +121,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = 'media/'
+
+if DEBUG:
+    STATIC_ROOT = BASE_DIR / 'static/'
+    MEDIA_ROOT = BASE_DIR / 'media/'
+
+    STATICFILES_DIRS = [
+        'base/static',
+    ]
+else:
+    STATIC_ROOT = ''
+    MEDIA_ROOT = ''
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
